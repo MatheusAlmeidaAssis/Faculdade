7bf910702060a12ea329dc765c15a112c3201f3d
MatheusAlmeidaAssis <m.almeidadeassis@gmail.com>
2019-08-22 15:37:20 -0300

    inicio do projeto

---
+++ Dos.UI/Controls/DataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Dos.UI.Controls
+{
+    sealed class DataBase : IDisposable
+    {        
+        static DataBase _instancia;
+        public SqlConnection Con { get;}
+        public static DataBase GetDataBase
+        {
+            get { return _instancia ?? (_instancia = new DataBase()); }
+        }
+        public DataBase()
+        {
+            Con = new SqlConnection(ConfigurationManager.ConnectionStrings["Faculdade"].ConnectionString);
+            Con.Open();
+        }
+        public void Dispose()
+        {
+            if (Con.State == ConnectionState.Open)
+                Con.Close();
+        }
+    }
+}
---
+++ Dos.UI/Controls/Insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dos.UI.Controls
+{
+    class Insert
+    {        
+        public List<string> Campos { get; set;}
+        public string Tabela { get; set; }
+        public List<string> Valores { get; set; }
+        public Insert()
+        {
+            Campos = new List<string>();
+            Tabela = string.Empty;
+            Valores = new List<string>();
+        }
+        public void Executar()
+        {
+
+        }        
+    }
+}
