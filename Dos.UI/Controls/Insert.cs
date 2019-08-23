ca510d9fa969e3b3920694112f1f2b59024e9949
MatheusAlmeidaAssis <m.almeidadeassis@gmail.com>
2019-08-22 22:59:53 -0300

    Select.cs e Insert.cs

--- Dos.UI/Controls/Insert.cs
+++ Dos.UI/Controls/Insert.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
 
 namespace Dos.UI.Controls
 {
     class Insert
-    {        
-        public List<string> Campos { get; set;}
+    {
+        private SqlCommand _cmdInsert;
+        public List<string> Campos { get; set; }
         public string Tabela { get; set; }
         public List<string> Valores { get; set; }
         public Insert()
@@ -17,9 +16,17 @@ namespace Dos.UI.Controls
             Tabela = string.Empty;
             Valores = new List<string>();
         }
+
+        public SqlCommand Command()
+        {
+            if (_cmdInsert == null)
+                _cmdInsert = new SqlCommand(Utils.MontaInsert(Campos, Tabela, Valores),
+                    DataBase.GetDataBase.Con);
+            return _cmdInsert;
+        }
         public void Executar()
         {
-
-        }        
+           this.Command().ExecuteNonQuery();
+        }
     }
 }
---
+++ Dos.UI/Controls/Select.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dos.UI.Controls
+{
+    class Select
+    {
+        private SqlCommand _cmdSelect;
+        public List<List<string>> Campos { get; set; }
+        public List<List<string>> Tabelas { get; set; }
+        public List<List<string>> LeftJoin { get; set; }
+        public List<List<string>> InnerJoin { get; set; }
+        public List<List<string>> RightJoin { get; set; }
+        public List<List<string>> FullOuterJoin { get; set; }
+        public List<List<string>> Where { get; set; }
+        public List<List<string>> GroupBy { get; set; }
+        public List<string> OrderBy { get; set; }
+
+        public Select()
+        {
+            Campos = new List<List<string>>();
+            Tabelas = new List<List<string>>();
+        }
+        public SqlCommand Command()
+        {
+            if (_cmdSelect == null)
+                _cmdSelect = new SqlCommand(Utils.MontaSelect(Campos, Tabelas, LeftJoin, InnerJoin,
+                    RightJoin, FullOuterJoin, Where, GroupBy, OrderBy), DataBase.GetDataBase.Con);
+            return _cmdSelect;
+        }
+        public SqlDataReader DataReader()
+        {
+            return this.Command().ExecuteReader();
+        }
+    }
+}
